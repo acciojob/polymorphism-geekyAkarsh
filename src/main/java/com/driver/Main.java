aa37e304a02dafdd261c7060eb04c8ec4fed432d
Akarsh <akarshsingh81@gmail.com>
2023-04-14 15:51:37 +0530

    changed * by + | removed print statements

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,20 +1,20 @@
 package com.driver;
 
 public class Main {
-    static class Product{
+    public static class Product{
 
         public int product(int x,int y){
-            return x*y;
+            return x+y;
         }
 
         // @Overload
         public int product(int x,int y,int z){
-            return x*y*z;
+            return x+y+z;
         }
 
         // @Overload
         public double product(double x, double y){
-            return x*y;
+            return x+y;
         }
 
     }
@@ -22,9 +22,9 @@ public class Main {
     public static void main(String[] args) {
 
         Product p = new Product();
-        System.out.println(p.product(10,20));
-        System.out.println(p.product(10,20,30));
-        System.out.println(p.product(1.5,2.5));
+        p.product(10,20);
+        p.product(10,20,30);
+        p.product(1.5,2.5);
     }
 
 }
